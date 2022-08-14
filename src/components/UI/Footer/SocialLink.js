a3708286525059b11c9128cb4ffa6c49e7caa4ab
Zhan Ostapko <84698801+zhanostapko@users.noreply.github.com>
2022-08-14 17:32:11 +0300

    Update SocialLink.js

--- src/components/UI/Footer/SocialLink.js
+++ src/components/UI/Footer/SocialLink.js
@@ -3,7 +3,7 @@ import classes from './SocialLink.module.css';
 
 const SocialLink = ({ link, icon }) => {
   return (
-    <a href={link} target="_blank">
+    <a href={link} target="_blank" rel="noreferrer">
       <img className={classes.icon} src={icon} alt="" />
     </a>
   );
