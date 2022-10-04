35f791043324a7f4183dbc722c56857b833f291c
ZAO <zans.ostapko@smartlynx.aero>
2022-10-04 11:45:21 +0300

    development branch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import TableWrapper from './components/General/TableWrapper';
-import './App.css';
-import NotFound from './components/UI/NotFound/NotFound';
-import Header from './components/UI/Header/Header';
-import Detail from './components/Detail/Detail';
-import Footer from './components/UI/Footer/Footer';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import TableWrapper from "./components/General/TableWrapper";
+import "./App.css";
+import NotFound from "./components/UI/NotFound/NotFound";
+import Header from "./components/UI/Header/Header";
+import Detail from "./components/Detail/Detail";
+import Footer from "./components/UI/Footer/Footer";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+
+      <h1>Development branch testing</h1>
       <Routes>
         <Route path="/" element={<TableWrapper />} />
         <Route path="/:invoiceId" element={<Detail />} />
